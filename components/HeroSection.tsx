21f88518cc34b9b8af3c9c4870e19e6fce794a38
andresCamp <andreslcampos@icloud.com>
2023-09-07 09:27:00 -0600

    apos r3

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,7 +5,7 @@ import ParticlesContainer from './ParticlesContainer'
 const HeroSection = () => {
   return (
     <div className=' flex min-h-screen flex-col gap-8 items-center justify-center '>
-      <h1 className='text-4xl w-1/2 text-center'>Unlock AI's potential for your business. Drop your email; let's explore together.</h1>
+      <h1 className='text-4xl w-1/2 text-center'>Unlock AI&apos;s potential for your business. Drop your email; let&apos;s explore together.</h1>
 
         <div className='z-50'>
             <FormsparkEmailCapture />
